644bd1a6cc34207c872ac6fd19cdf9d7cdd55675
unknown <alison.souza.707@ufrn.edu.br>
2025-08-17 13:50:54 -0300

    feat(app): add currency formatter to utils

--- resources/js/lib/utils.ts
+++ resources/js/lib/utils.ts
@@ -4,3 +4,16 @@ import { twMerge } from 'tailwind-merge';
 export function cn(...inputs: ClassValue[]) {
     return twMerge(clsx(inputs));
 }
+
+export function formatCurrency(value: string, currency: 'usd' | 'brl' | 'eur') {
+    const locales = {
+        usd: 'en-US',
+        eur: 'de-DE',
+        brl: 'pt-BR'
+    }
+
+    return new Intl.NumberFormat(locales[currency], {
+        style: 'currency',
+        currency: currency.toUpperCase()
+    }).format(Number(value))
+}
