99cd994ea8892913283382df6f282b1cf05e3b6c
unknown <alison.souza.707@ufrn.edu.br>
2025-08-17 13:51:20 -0300

    feat(ui): add icon option to badge

--- resources/js/components/ui/badge/Badge.vue
+++ resources/js/components/ui/badge/Badge.vue
@@ -1,35 +1,52 @@
 <script setup lang="ts">
     import { computed } from 'vue';
 
-    const props = defineProps(['status'])
+    const props = defineProps(['status', 'icon'])
+
+    const statusClasses: Record<string, string[]> = {
+        Completed: [
+            'border-green-600',
+            'text-green-600',
+            'hover:bg-green-600',
+            'dark:bg-green-700'
+        ],
+        Active: [
+            'border-blue-600',
+            'text-blue-600',
+            'hover:bg-blue-600',
+            'dark:bg-blue-950'
+        ],
+        Inactive: [
+            'border-red-600',
+            'text-red-600',
+            'hover:bg-red-600',
+            'dark:bg-red-950'
+        ],
+        Pending: [
+            'border-amber-500',
+            'text-amber-500',
+            'hover:bg-amber-500',
+            'dark:bg-amber-900'
+        ],
+        Default: [
+            'border-gray-500',
+            'text-gray-500',
+            'hover:bg-gray-500',
+            'dark:bg-gray-700'
+        ]
+    }
 
     const classes = computed(() => {
-        const classList = ['badge', 'rounded-sm']
-
-        switch(props.status) {
-            case 'Completed':
-                classList.push('bg-green-700')
-                break
-            case 'Active':
-                classList.push('bg-green-950')
-                break
-            case 'Inactive':
-                classList.push('bg-red-950')
-                break
-            case 'Pending':
-                classList.push('bg-amber-900')
-                break
-            default:
-                classList.push('bg-gray-700')
-        }
-        return classList
+        const base = ['badge', 'rounded-sm', 'border', 'hover:text-white', 'dark:text-white', '']
+        return [...base, ...(statusClasses[props.status] ?? statusClasses.Default)]
     })
 </script>
 
 <template>
-    <span :class="classes">
-        {{ status }}
-    </span>
+    <div :class="classes">
+        <component v-if="icon" :size="16" :is="icon" class="flex md:hidden"/>
+        <span class="hidden md:inline">{{ status }}</span>
+    </div>
 </template>
 
 <style scoped>
