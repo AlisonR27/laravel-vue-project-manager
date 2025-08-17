e32846621bda8ce19f4ab0882573c1ee5eb941c6
unknown <alison.souza.707@ufrn.edu.br>
2025-08-17 03:21:27 -0300

    feat(ui): Add badge component

---
+++ resources/js/components/ui/badge/Badge.vue
@@ -0,0 +1,41 @@
+<script setup lang="ts">
+    import { computed } from 'vue';
+
+    const props = defineProps(['status'])
+
+    const classes = computed(() => {
+        const classList = ['badge', 'rounded-sm']
+
+        switch(props.status) {
+            case 'Completed':
+                classList.push('bg-green-700')
+                break
+            case 'Active':
+                classList.push('bg-green-950')
+                break
+            case 'Inactive':
+                classList.push('bg-red-950')
+                break
+            case 'Pending':
+                classList.push('bg-amber-900')
+                break
+            default:
+                classList.push('bg-gray-700')
+        }
+        return classList
+    })
+</script>
+
+<template>
+    <span :class="classes">
+        {{ status }}
+    </span>
+</template>
+
+<style scoped>
+.badge {
+    padding: 4px 10px;
+    box-sizing: content-box;
+    text-align: center;
+}
+</style>
