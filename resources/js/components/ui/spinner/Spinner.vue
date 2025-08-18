601b341ae8c16404a43518dd77dc1956973902a5
unknown <alison.souza.707@ufrn.edu.br>
2025-08-17 22:41:05 -0300

    feat(ui): Add spinner component

---
+++ resources/js/components/ui/spinner/Spinner.vue
@@ -0,0 +1,41 @@
+<!--
+Source: https://cssloaders.github.io/
+-->
+<script setup lang="ts">
+</script>
+
+<template>
+    <span class="loader block before:border-4 before:border-neutral-600 dark:before:border-neutral-300"></span>
+</template>
+
+<style scoped>
+
+.loader {
+    display: block;
+    width: 48px;
+    height: 48px;
+    border-radius: 50%;
+    position: relative;
+    animation: rotate 1s linear infinite
+}
+.loader::before {
+    content: "";
+    box-sizing: border-box;
+    position: absolute;
+    inset: 0px;
+    border-radius: 50%;
+    animation: prixClipFix 3s linear infinite ;
+}
+
+@keyframes rotate {
+    100%   {transform: rotate(360deg)}
+}
+
+@keyframes prixClipFix {
+    0%   {clip-path:polygon(50% 50%,0 0,0 0,0 0,0 0,0 0)}
+    25%  {clip-path:polygon(50% 50%,0 0,100% 0,100% 0,100% 0,100% 0)}
+    50%  {clip-path:polygon(50% 50%,0 0,100% 0,100% 100%,100% 100%,100% 100%)}
+    75%  {clip-path:polygon(50% 50%,0 0,100% 0,100% 100%,0 100%,0 100%)}
+    100% {clip-path:polygon(50% 50%,0 0,100% 0,100% 100%,0 100%,0 0)}
+}
+</style>
