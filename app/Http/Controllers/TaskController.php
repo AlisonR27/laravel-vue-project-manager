63996052b3024e74bb0ebe9c5424dfe25362e21d
unknown <alison.souza.707@ufrn.edu.br>
2025-08-18 13:07:27 -0300

    feat(backend): add filters to task index

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -3,17 +3,55 @@
 namespace App\Http\Controllers;
 
 use App\Models\Task;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
 
 class TaskController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-        $tasks = Task::select('id', 'title', 'status', 'project_id', 'updated_at')->with(['project' =>  function ($query) {
-            $query->select('id', 'name');
-        }])->get();
+        $page_size = $request->query('page_size', 10);
 
+        $title = $request->query('title');
+        $status = $request->query('status');
+        $due_date = $request->query('end_date');
+        $creator_id = $request->query('creator_id');
+        $project_id = $request->query('project_id');
+
+        $query = Task::select('id', 'title', 'status', 'updated_at', 'project_id')
+            ->with('project');
+
+        if($title) $query->where('tasks.title', 'like', '%'.$title.'%');
+        if($status) $query->where('tasks.status', $status);
+        if($due_date) $query->where('tasks.due_date', '<=', Carbon::parse($due_date));
+        if($project_id) $query->where('project_id', $project_id);
+        if($creator_id) {
+            $query->join('projects', 'projects.id', '=', 'tasks.project_id');
+            $query->join('users', 'users.id', '=', 'projects.creator_id');
+            $query->where('users.id', $creator_id);
+            $query->select([
+                'tasks.*',
+                'projects.name as project_name',
+                'projects.id as project_id',
+                'users.name as user_name',
+                'users.id as user_id',
+            ]);
+        }
+
+        $tasks =$query->paginate($page_size)
+            ->through(fn ($task) => [
+                'id' => $task->id,
+                'title' => $task->title,
+                'status' => $task->status,
+                'updated_at' => $task->updated_at,
+                'project' => $task->project,
+                'can' => [
+                    'view' => true,
+                    'update' => auth()->user()->can('update', $task),
+                    'delete' => auth()->user()->can('delete', $task),
+                ]
+        ]);
         return Inertia::render('Task/List.All', [
             'tasks' => $tasks,
         ]);
