f7aaff41e2563891c267d01d1c4128c5aaea7ab2
unknown <alison.souza.707@ufrn.edu.br>
2025-08-17 22:44:23 -0300

    chore(app): add pinia and stores

--- resources/js/app.ts
+++ resources/js/app.ts
@@ -6,6 +6,7 @@ import type { DefineComponent } from 'vue';
 import { createApp, h } from 'vue';
 import { ZiggyVue } from 'ziggy-js';
 import { initializeTheme } from './composables/useAppearance';
+import pinia from './store'
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
@@ -13,9 +14,11 @@ createInertiaApp({
     title: (title) => (title ? `${title} - ${appName}` : appName),
     resolve: (name) => resolvePageComponent(`./pages/${name}.vue`, import.meta.glob<DefineComponent>('./pages/**/*.vue')),
     setup({ el, App, props, plugin }) {
+
         createApp({ render: () => h(App, props) })
             .use(plugin)
             .use(ZiggyVue)
+            .use(pinia)
             .mount(el);
     },
     progress: {
---
+++ resources/js/store/index.ts
@@ -0,0 +1,6 @@
+import { createPinia } from 'pinia'
+
+// cria e exporta o pinia
+const pinia = createPinia()
+
+export default pinia
